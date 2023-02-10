1f67aa6d95b69b1a78313dc6abae5abc05c430a3
eyasudev <mitchelljack979@gmail.com>
2023-02-11 01:59:46 +0200

    feat: add the animated hamburger button and the mobile menu for the Navbar

--- js/script.js
+++ js/script.js
@@ -1,8 +1,19 @@
+const btn = document.getElementById("menu-btn");
+const menu = document.getElementById("menu");
+
 const linkForm = document.getElementById("link-form");
 const input = document.getElementById("link-input");
 const errMsg = document.getElementById("err-msg");
 
+btn.addEventListener("click", navToggle);
 linkForm.addEventListener("submit", formSubmit);
+
+// Toggle Mobile Menu
+function navToggle() {
+  btn.classList.toggle("open");
+  menu.classList.toggle("flex");
+  menu.classList.toggle("hidden");
+}
 
 // Validate a URL
 function validURL(str) {
